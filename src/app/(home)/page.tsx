41e6939adeb83bfe3ae01905eb33c7a47ec69c05
ChehAchraf <achrafchehboun@outlook.com>
2025-07-19 21:07:18 +0100

    adding the navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,18 +6,7 @@ import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <div>
-        <Button variant="elevated">I'am a button</Button>
-      </div>
-      <div>
-        <Input placeholder="i'm an input"/>
-      </div>
-      <div>
-        <Progress value={33}></Progress>
-      </div>
-      <div>
-        <Textarea placeholder="enter the text" />
-      </div>
+      
     </div>
   );
 }
