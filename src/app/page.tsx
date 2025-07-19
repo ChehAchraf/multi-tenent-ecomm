3384e81b02b017fc8cbfa8d57566fd6302df45f6
ChehAchraf <achrafchehboun@outlook.com>
2025-07-19 20:27:11 +0100

    update the style of tailwind and also some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
-    <p className="">
-      hello
-    </p>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">I'am a button</Button>
+      </div>
+      <div>
+        <Input placeholder="i'm an input"/>
+      </div>
+      <div>
+        <Progress value={33}></Progress>
+      </div>
+      <div>
+        <Textarea placeholder="enter the text" />
+      </div>
+    </div>
   );
 }
